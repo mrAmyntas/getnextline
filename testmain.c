cb74deeeb9acebc15c5f71696e5dc06c4025c589
mrAmyntas <basz_h@hotmail.com>
2023-09-06 16:55:50 +0200

    gnl

---
+++ testmain.c
@@ -0,0 +1,29 @@
+
+#include "get_next_line.h"
+
+int   main(int ac, char **av)
+{
+  char  *line;
+  int   fd1;
+  int   fd2;
+
+  fd1 = open("file1", O_RDONLY);
+  fd2 = open("file2", O_RDONLY);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  get_next_line(fd1, &line);
+  printf("%s\n", line);
+  get_next_line(fd2, &line);
+  printf("%s\n", line);
+  return (0);
+}
